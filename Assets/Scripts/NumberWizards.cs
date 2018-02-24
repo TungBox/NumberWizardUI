7d39cfb89f8ed8b96cdd401d612ae3b436b14f67
TungBox <33808069+TungBox@users.noreply.github.com>
2018-02-24 18:43:34 +0700

    Add files via upload

---
+++ Assets/Scripts/NumberWizards.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class NumberWizards : MonoBehaviour {
+	// Use this for initialization
+	int max;
+	int min;
+	int guess;
+
+	int maxGuessesAllowed = 5;
+
+            public Text text;
+	void Start () {
+		StartGame();
+	}
+	
+	void StartGame () {
+		max = 1001;
+		min = 1;
+		NextGuess();
+		max = max + 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+	public void GuessHigher () {
+		min = guess;
+	    NextGuess();
+	}
+	public void GuessLower () {
+	    max = guess;
+	    NextGuess();
+	}
+	void NextGuess ()
+	{
+		guess = Random.Range(min, max+1);
+		text.text = guess.ToString();
+		maxGuessesAllowed = maxGuessesAllowed - 1;
+		if (maxGuessesAllowed <= 0) {
+		   SceneManager.LoadScene("Win");
+		}
+	}
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+     public void LoadGame ()
+	{
+	   SceneManager.LoadScene("Game");
+	}
+	public void LoadStart ()
+	{
+	   SceneManager.LoadScene("Start");
+	}
+	public void LoadWin ()
+	{
+		SceneManager.LoadScene ("Win");
+	}
+	public void LoadLose ()
+	{
+		SceneManager.LoadScene ("Lose");
+	}
+}
